15587162f2d5de815ac27a44792848d8b8eead02
reimo85 <54685018+reimo85@users.noreply.github.com>
2019-11-09 17:05:19 -0500

    Update SimpleBufferOverFlowFuzzer.py

--- SimpleBufferOverFlowFuzzer.py
+++ SimpleBufferOverFlowFuzzer.py
@@ -1,21 +1,27 @@
 import sys, socket, time
  
 # Use in the form "python fuzzer.py  "
+
+# Recieve IP from user
+host = sys.argv[1]
+
+# Recieve Port from user
+port = int(sys.argv[2]) 					                         
+
+# Initial length of 100 A's
+length = 100 							                                  
+
+# Stop once we've tried up to 3000 length
+while (length &lt; 3000): 		
  
-host = sys.argv[1] 						                             # Recieve IP from user
-port = int(sys.argv[2]) 					                         # Recieve Port from user
- 
-length = 100 							                                  # Initial length of 100 A's
- 
-while (length &lt; 3000): 					                       # Stop once we've tried up to 3000 length
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 	# Declare a TCP socket
-client.connect((host, port)) 					                    # Connect to user supplied port and IP address
-client.recv(1024) 						                              # Recieve FTP Banner
-client.send("USER " + "A" * length) 				              # Send the user command with a variable length name
-client.recv(1024) 						                              # Recieve Reply
-client.send("PASS pass") 					                        # Send pass to complete connection attempt (will fail)
-client.recv(1024) 						                              # Recieve Reply
-client.close() 							                                # Close the Connection
-time.sleep(2) 							                                 # Sleep to prevent DoS crashes
-print "Length Sent: " + str(length) 				              # Output the length username sent to the server
-length += 100 						                                	 # Try again with an increased length
+client.connect((host, port)) 					                          # Connect to user supplied port and IP address
+client.recv(1024) 						                                    # Recieve FTP Banner
+client.send("USER " + "A" * length) 				                    # Send the user command with a variable length name
+client.recv(1024) 						                                    # Recieve Reply
+client.send("PASS pass") 					                              # Send pass to complete connection attempt (will fail)
+client.recv(1024) 						                                    # Recieve Reply
+client.close() 							                                      # Close the Connection
+time.sleep(2) 							                                       # Sleep to prevent DoS crashes
+print "Length Sent: " + str(length) 				                    # Output the length username sent to the server
+length += 100 						                                	       # Try again with an increased length
